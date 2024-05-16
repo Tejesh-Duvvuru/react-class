b748b9e4a3ee2defbbfd5e0b0c65a52a2e020805
Tejesh-Duvvuru <saitejesh1919@gmail.com>
2024-05-16 13:02:58 +0530

    update class 3

--- App.js
+++ App.js
@@ -3,20 +3,45 @@ import ReactDOM from "react-dom/client";
 
 //function component is normal js function which returns jsx
 //Write first letter is Capital to understand
-const HeadingComponent = () => {
+const Title = () => {
   return <h1>Welcome to the react world</h1>;
 };
-// HeadingComponent and HeadingComponent1 both are same
-const HeadingComponent1 = () => (
+
+let api = {
+  getData: () => {
+    return "assuem from api";
+  },
+};
+//jsx
+const title = <h1>Pure js function</h1>;
+const data = api.getData();
+//here data is inject in jsx, suppose or assume api gives bad data attackers send some js code in data thorugh api,
+// the js code will run in your browser what happens, they can steel your data, this type is called x cross site attacks
+//don't worry jsx it self prevent or sanitize the data (if you want you can read more)
+// HeadingComponent and Title both are same (instead of return we use round brases)
+const HeadingComponent = () => (
   <div>
-    <HeadingComponent />
+    <Title />
+    <Title></Title>
+    {Title()}
+    {/* <Title />
+    <Title></Title>
+    {Title()}  at end Function component is a normal function , inside {we can call function}
+    all are same */}
+    {/* INJECTING JS {} */}
+    {title}
+    {data}
+
     <h1>I am js I am your boss</h1>
+    <h6>{2 + 3}</h6>
   </div>
 );
+
+// if we want we can also use normal functions instead of arrow functions
 
 //what is component composition
 //calling one function inside another function
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //for render component we need to pass <component name/>
-root.render(<HeadingComponent1 />);
+root.render(<HeadingComponent />);
